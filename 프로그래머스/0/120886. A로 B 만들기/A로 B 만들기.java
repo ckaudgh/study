5425432bb69d2c02b4b5fb136094817fa03861f0
myungho <166399338+ckaudgh@users.noreply.github.com>
2025-01-31 16:45:39 +0900

    [level 0] Title: A로 B 만들기, Time: 1.05 ms, Memory: 79.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120886. A로 B 만들기/A로 B 만들기.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(String before, String after) {
+        int answer = 0;
+        char[] bef = before.toCharArray();
+        char[] aft = after.toCharArray();
+        
+        Arrays.sort(bef);
+        Arrays.sort(aft);
+        if(Arrays.equals(bef,aft)) answer = 1;
+        else answer = 0;
+        return answer;
+    }
+}
