4e945909d5c326f8cb52122ddddb1f1f0d08ec0c
myungho <166399338+ckaudgh@users.noreply.github.com>
2025-02-18 11:28:03 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.03 ms, Memory: 89.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int[] numbers) {
+        int answer = 0;
+        int sum = 0;
+        for(int i = 1; i<=9;i++)sum+=i;
+        for(int i = 0; i<numbers.length; i++) answer += numbers[i];
+        answer = sum - answer;
+        return answer;
+    }
+}
