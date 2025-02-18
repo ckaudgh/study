a0a55f625a323069532b657577c81eaa29b17007
myungho <166399338+ckaudgh@users.noreply.github.com>
2025-02-18 10:13:40 +0900

    [level 1] Title: 하샤드 수, Time: 0.17 ms, Memory: 91.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean solution(int x) {
+        boolean answer = true;
+        String[] str = Integer.toString(x).split("");
+        int sum = 0;
+        for(int i = 0; i < str.length; i++){
+            sum+=Integer.parseInt(str[i]);
+        }
+        if(x%sum != 0) answer = false;
+        return answer;
+    }
+}
