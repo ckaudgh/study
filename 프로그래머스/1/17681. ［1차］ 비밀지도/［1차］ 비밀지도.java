6d86f16627920db3feaecdfb94910c729bc3c2ff
myungho <166399338+ckaudgh@users.noreply.github.com>
2025-02-26 17:23:00 +0900

    [level 1] Title: [1차] 비밀지도, Time: 11.33 ms, Memory: 73.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -0,0 +1,31 @@
+class Solution {
+    // binary의 길이가 n보다 작을 경우 앞에 부족한 만큼 0추가
+    public String[] addZero(String bin, int n){
+        if(bin.length()!=n){
+            String zero = "";
+            for(int i=0; i<n-bin.length(); i++) {
+                zero += "0";
+            }
+            bin = zero + bin;
+        }
+        return bin.split("");
+    }
+    public String[] solution(int n, int[] arr1, int[] arr2) {
+        String[] answer = new String[n];
+        for(int i = 0; i < n; i++){
+            String row = "";
+            String bin1 = Integer.toBinaryString(arr1[i]);
+            String bin2 = Integer.toBinaryString(arr2[i]);
+            
+            String[] binary1 = addZero(bin1,n);
+            String[] binary2 = addZero(bin2,n);
+            
+            for(int j = 0; j < n; j++){
+                if(binary1[j].equals("1")||binary2[j].equals("1")) row += "#";
+                else row += " ";
+            }
+            answer[i] = row;
+        }
+        return answer;
+    }
+}
