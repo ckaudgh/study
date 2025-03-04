ab71568c147a3b41586f6b47e9e9bc105c5c81ba
myungho <166399338+ckaudgh@users.noreply.github.com>
2025-03-05 01:47:02 +0900

    [level 1] Title: 소수 찾기, Time: 201.10 ms, Memory: 54.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.java
@@ -0,0 +1,19 @@
+class Solution {
+    public static boolean isPrime(int num){
+        for(int i=2; i*i<=num; i++){
+            if(num % i == 0) return false;
+        }
+        return true;
+    }
+    
+    public int solution(int n) {
+        int answer = 0;
+        for(int i=2; i<=n; i++) {
+            if(this.isPrime(i)) {
+                answer++;
+            }   
+        }
+        
+        return answer;
+    }
+}
