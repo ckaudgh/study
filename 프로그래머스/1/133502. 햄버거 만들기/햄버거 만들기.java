7883e679b6b63149c059db2c0d375b69c6ec2963
myungho <166399338+ckaudgh@users.noreply.github.com>
2025-03-13 11:19:32 +0900

    [level 1] Title: 햄버거 만들기, Time: 14.04 ms, Memory: 142 MB -BaekjoonHub

--- 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.java
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.java
@@ -1,26 +1,46 @@
-import java.util.*;
+// import java.util.*;
+// class Solution {
+//     public int solution(int[] ingredient) {
+//        int answer = 0;
+// 		List<Integer> list = new ArrayList<>();
+		
+// 		for(int i :ingredient) {
+// 			list.add(i);
+//             // list의 크기가 4 이상이면 반복
+// 			while(list.size() >= 4) {
+// 				int s = list.size();
+//                 // 1 2 3 1 순서가 아니면 break
+// 				if(!(list.get(s-1)==1
+// 					&& list.get(s-2)==3
+// 					&& list.get(s-3)==2
+// 					&& list.get(s-4)==1)) break;
+//                 // 순서가 맞으면 뒤에서 부터 remove
+// 				for(int j=0; j<4; j++) {
+// 					list.remove(list.size() -1);
+// 				}
+// 				answer++;
+// 			}
+// 		}
+//         return answer;
+//     }
+// }
+
 class Solution {
     public int solution(int[] ingredient) {
-       int answer = 0;
-		List<Integer> list = new ArrayList<>();
-		
-		for(int i :ingredient) {
-			list.add(i);
-            // list의 크기가 4 이상이면 반복
-			while(list.size() >= 4) {
-				int s = list.size();
-                // 1 2 3 1 순서가 아니면 break
-				if(!(list.get(s-1)==1
-					&& list.get(s-2)==3
-					&& list.get(s-3)==2
-					&& list.get(s-4)==1)) break;
-                // 순서가 맞으면 뒤에서 부터 remove
-				for(int j=0; j<4; j++) {
-					list.remove(list.size() -1);
-				}
-				answer++;
-			}
-		}
+        int[] stack = new int[ingredient.length];
+        int sp = 0;
+        int answer = 0;
+        for (int i : ingredient) {
+            stack[sp++] = i;
+            if (sp >= 4 && stack[sp-1] == 1
+                && stack[sp - 2] == 3
+                && stack[sp - 3] == 2
+                && stack[sp - 4] == 1) {
+                sp -= 4;
+                answer++;
+            }
+        }
         return answer;
     }
 }
+
